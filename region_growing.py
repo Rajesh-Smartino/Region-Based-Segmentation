93857af685afaddb914f9823fa8ae760ee36cdc3
Rajesh <rajesh.krsk@gmail.com>
2021-12-11 11:57:03 +0530

    updated code

--- region_growing.py
+++ region_growing.py
@@ -1,24 +1,80 @@
-from PIL import Image
-from pylab import *
-from pylab import *
 import cv2
+from pylab import *
 
-image = cv2.imread("/Users/rajeshr/Desktop/ProjectDIP/view4.png", cv2.IMREAD_GRAYSCALE)
-temp_kernel = np.asarray(image)
 
-# hsv_image = cv2.imread("/Users/rajeshr/Desktop/ProjectDIP/peppers.png", cv2.COLOR_BGR2HSV)
-# image = hsv_image[:, :, 0]
-# temp_kernel = np.asarray(image)
+def growregion(temp_kernel, seed_pixel):
+    row, col = np.shape(temp_kernel)
+    region_grow_image = np.zeros((row + 1, col + 1))
+    region_grow_image[seed_pixel[0]][seed_pixel[1]] = 255.0
 
-row, col = np.shape(temp_kernel)
-plt.figure()
-plt.imshow(image)
-plt.gray()
+    region_points = [[seed_pixel[0], seed_pixel[1]]]
 
-seed_point = plt.ginput(1)
-x = int(seed_point[0][0])
-y = int(seed_point[0][1])
-seed_pixel = [x, y]
+    count = 0
+    x = [-1, 0, 1, -1, 1, -1, 0, 1]
+    y = [-1, -1, -1, 0, 0, 1, 1, 1]
 
-print('you clicked:', seed_pixel)
-plt.close()
+    while len(region_points) > 0:
+        if count == 0:
+            point = region_points.pop(0)
+            i = point[0]
+            j = point[1]
+            print('i and j:', i, j)
+        print('\nRunning.. Please Wait..')
+        intensity = temp_kernel[i][j]
+        low = intensity - 8
+        high = intensity + 8
+        for k in range(8):
+            if region_grow_image[i + x[k]][j + y[k]] != 1:
+                try:
+                    if low < temp_kernel[i + x[k]][j + y[k]] < high:
+                        region_grow_image[i + x[k]][j + y[k]] = 1
+                        p = [0, 0]
+                        p[0] = i + x[k]
+                        p[1] = j + y[k]
+                        if p not in region_points:
+                            if 0 < p[0] < row and 0 < p[1] < col:
+                                ''' adding points to the region '''
+                                region_points.append([i + x[k], j + y[k]])
+                                print('Added Region: ', [i + x[k], j + y[k]])
+                    else:
+                        region_grow_image[i + x[k]][j + y[k]] = 0
+
+                except IndexError:
+                    continue
+
+        point = region_points.pop(0)
+        i = point[0]
+        j = point[1]
+        count = count + 1
+
+    return region_grow_image
+
+
+def regionSeg(path):
+    image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    temp_kernel = np.asarray(image)
+
+    plt.figure()
+    plt.imshow(image)
+    plt.gray()
+
+    print('Click on the region to grow')
+
+    seed_point = plt.ginput(1)
+    x = int(seed_point[0][0])
+    y = int(seed_point[0][1])
+
+    seed_pixel = [x, y]
+    print('Seed Points: ', seed_pixel)
+    plt.close()
+
+    region_grow_image = growregion(temp_kernel, seed_pixel)
+
+    plt.figure()
+    plt.imshow(region_grow_image, cmap="Greys_r")
+    plt.colorbar()
+    plt.show()
+
+
+path = "/Users/rajeshr/Desktop/ProjectDIP/rgb_shapes.jpeg"
+regionSeg(path)
