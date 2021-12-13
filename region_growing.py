7d87f3256195335952608ff399e22997d56c3362
Rajesh <rajesh.krsk@gmail.com>
2021-12-13 11:13:16 +0530

    Color Images added

--- region_growing.py
+++ region_growing.py
@@ -1,13 +1,24 @@
+'''
+Implementation of Region Growing
+Reference: R. C. Gonzalez and R. E. Woods, "Digital Image Processing" Third edition, Pearson Education, 2009
+'''
+
 import cv2
 from pylab import *
+
+'''
+Function: Accepts seed points to give the region grown out of the image.
+Inputs: Image as numpy array
+Returns: Output region image
+'''
 
 
 def growregion(temp_kernel, seed_pixel):
     row, col = np.shape(temp_kernel)
     region_grow_image = np.zeros((row + 1, col + 1))
 
-    print('temp kernel', temp_kernel.shape)
-    print('region_grow_image', region_grow_image.shape)
+    # print('temp kernel', temp_kernel.shape)
+    # print('region_grow_image', region_grow_image.shape)
     swap = [seed_pixel[1], seed_pixel[0]]
 
     region_grow_image[swap[0]][swap[1]] = 255.0
@@ -18,10 +29,10 @@ def growregion(temp_kernel, seed_pixel):
     yp = [-1, -1, -1, 0, 0, 1, 1, 1]
 
     print('Running.. Please Wait..')
-    count = 0
+    c = 0
     while len(region_points) > 0:
 
-        if count == 0:
+        if c == 0:
             point = region_points.pop(0)
             i = point[0]
             j = point[1]
@@ -43,17 +54,23 @@ def growregion(temp_kernel, seed_pixel):
                                 print('Added Region: ', [i + xp[k], j + yp[k]])
                     else:
                         region_grow_image[i + xp[k]][j + yp[k]] = 0
-
                 except IndexError:
                     continue
 
         point = region_points.pop(0)
         i = point[0]
         j = point[1]
-        count = count + 1
+        c = c + 1
 
     print('Region addition completed')
     return region_grow_image
+
+
+'''
+Function: Accepts image path (Gray scale image or it does).
+Inputs: Image Path
+Returns: Segmented Region Grown Image
+'''
 
 
 def regionSeg(path):
@@ -61,6 +78,7 @@ def regionSeg(path):
     temp_kernel = np.asarray(image)
 
     plt.figure()
+    plt.title('Input Image\nSelect a seed point to segment a region')
     plt.imshow(image)
     plt.gray()
 
@@ -77,10 +95,84 @@ def regionSeg(path):
     region_grow_image = growregion(temp_kernel, seed_pixel)
 
     plt.figure()
-    plt.imshow(region_grow_image, cmap="Greys_r")
-    plt.colorbar()
+    plt.title('Resulted Segmented Image')
+    plt.imshow(region_grow_image, cmap="gray")
     plt.show()
 
 
+'''
+Function: Accepts image path (Color Images).
+Inputs: Image Path
+Returns: Segmented Region Grown Image
+'''
+
+
+def colorRegionSeg(path):
+    # Tried with YCbCr and HSV color models but the size of the\
+    # image is reduced abruptly and selection of seed  points becomes\
+    # difficult (selected in new range). So sticked with RGB model itself, results are satisfactory\
+    # than above models.
+
+    # Implementing YCbCr and HSV is same and we maintained dynamic change\
+    # to try  YCbCr or HSV uncomment the codes and comment the RGB part to\
+    # test the code.
+    
+    image = cv2.imread(path)
+    (B, G, R) = cv2.split(image)
+
+    # image = cv2.imread(path, cv2.COLOR_BGR2YCrCb)
+    # (Y, Cr, Cb) = cv2.split(image)
+
+    plt.figure()
+    plt.title('Input Image\nSelect a seed point to segment a region')
+    plt.imshow(image)
+    plt.gray()
+
+    print('Click on the region to grow')
+
+    seed_point = plt.ginput(1)
+    x = int(seed_point[0][0])
+    y = int(seed_point[0][1])
+
+    seed_pixel = [x, y]
+    print('Seed Points: ', seed_pixel)
+    plt.close()
+
+    temp_kernel1 = np.asarray(R)
+    temp_kernel2 = np.asarray(G)
+    temp_kernel3 = np.asarray(B)
+
+    Rregion = growregion(temp_kernel1, seed_pixel)
+    Gregion = growregion(temp_kernel2, seed_pixel)
+    Bregion = growregion(temp_kernel3, seed_pixel)
+
+    region_grow_image = cv2.merge([Bregion, Gregion, Rregion])
+
+    # temp_kernel1 = np.asarray(Y)
+    # temp_kernel2 = np.asarray(Cb)
+    # temp_kernel3 = np.asarray(Cr)
+
+    # Yregion = growregion(temp_kernel1, seed_pixel)
+    # Cbregion = growregion(temp_kernel2, seed_pixel)
+    # Crregion = growregion(temp_kernel3, seed_pixel)
+
+    # region_grow_image = cv2.merge([Yregion, Cbregion, Crregion]) #Or
+    # region_grow_image = Yregion
+
+    plt.figure()
+    plt.title('Resulted Segmented Image')
+    plt.imshow(region_grow_image)
+    plt.show()
+
+
+'''
+Main function.
+Change the path to accept images (Accepted: .jpg, .jpeg, .png, .bmp)
+'''
 path = "/Users/rajeshr/Desktop/ProjectDIP/shapes.jpeg"
+
+# Uncomment for gray scale processing or Comment for color image. (Preferred this)
 regionSeg(path)
+
+# Uncomment for color image and Comment for gray scale images
+# colorRegionSeg(path)
